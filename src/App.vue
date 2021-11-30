0fead0970787e2ff81d4ab54cb81cc989a5591ba
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-11-30 13:02:00 +0100

    call to api and consequent logic moved into Main

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="app">
     <Header :genres="genresList" @sendOption="getOption"/>
-    <Main :discs="filteredList"/>
+    <Main :genre="selectedGenre" @sendGenreList="getGenreList"/>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
 import Main from './components/Main/Main.vue';
 import Header from './components/Header/Header.vue';
 
@@ -18,52 +17,16 @@ export default {
   },
   data() {
     return {
-      discsList: [],
       genresList: [],
       selectedGenre: '',
     }
   },
-  computed: {
-    filteredList() {
-      if (!this.selectedGenre) {
-        return this.discsList
-      }
-
-      return this.discsList.filter((disc) => {
-        return disc.genre.includes(this.selectedGenre);
-      });
-    }
-  },
-  created() {
-    this.getDiscs();
-  },
   methods: {
-    // getDiscs() {
-    //   axios.
-    //   get('https://flynn.boolean.careers/exercises/api/array/music')
-    //   .then((response) => {
-    //     this.discsList = response.data.response;
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   })
-    // }
-    getDiscs: async function() {
-      try {
-        let response = await axios.get('https://flynn.boolean.careers/exercises/api/array/music');
-        this.discsList = response.data.response;
-        this.genresList = this.discsList.reduce((acc, curr) => {
-          if (acc.length === 0 || !acc.includes(curr.genre)) {
-            acc.push(curr.genre);
-          }
-          return acc;
-        }, []);
-      } catch(error) {
-        console.log(error);
-      }
-    },
     getOption(value) {
       this.selectedGenre = value;
+    },
+    getGenreList(arr) {
+      this.genresList = arr;
     }
   }
 }
--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -2,13 +2,14 @@
   <div>
     <div class="container">
       <DiscCard
-      v-for="(disc, i) in discs" :key="i"
+      v-for="(disc, i) in filteredList" :key="i"
       :details="disc"/>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import DiscCard from "./DiscCard.vue";
 
 export default {
@@ -17,8 +18,55 @@ export default {
     DiscCard,
   },
   props: {
-    discs: Array
+    genre: String
   },
+  data() {
+    return {
+      discsList: [],
+      genresList: []
+    }
+  },
+  computed: {
+    filteredList() {
+      if (!this.genre) {
+        return this.discsList
+      }
+
+      return this.discsList.filter((disc) => {
+        return disc.genre.includes(this.genre);
+      });
+    }
+  },
+  created() {
+    this.getDiscs();
+    },
+  methods: {
+    // getDiscs() {
+    //   axios.
+    //   get('https://flynn.boolean.careers/exercises/api/array/music')
+    //   .then((response) => {
+    //     this.discsList = response.data.response;
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   })
+    // }
+    getDiscs: async function() {
+      try {
+        let response = await axios.get('https://flynn.boolean.careers/exercises/api/array/music');
+        this.discsList = response.data.response;
+        this.genresList = this.discsList.reduce((acc, curr) => {
+          if (acc.length === 0 || !acc.includes(curr.genre)) {
+            acc.push(curr.genre);
+          }
+          return acc;
+        }, []);
+        this.$emit('sendGenreList', this.genresList);
+      } catch(error) {
+        console.log(error);
+      }
+    },
+  }
 }
 </script>
 
